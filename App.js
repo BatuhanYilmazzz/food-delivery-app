ebc1e54f069a3ff52f27c25a6662fe40b24a524c
BatuhanYilmaz <batuhan@marsinvestment.com.tr>
2020-12-21 19:48:24 +0300

    added home page nav

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import {StyleSheet, Text, View} from 'react-native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {NavigationContainer} from '@react-navigation/native';
 
+import Tabs from './navigation/Tabs';
 import {Home, Restaurant, OrderDelivery} from './screens';
 
 const Stack = createStackNavigator();
@@ -15,7 +16,9 @@ const App = () => {
           headerShown: false,
         }}
         initialRouteName="Home">
-        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Home" component={Tabs} />
+        <Stack.Screen name="Restaurant" component={Restaurant} />
+        <Stack.Screen name="OrderDelivery" component={OrderDelivery} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- screens/Home.js
+++ screens/Home.js
@@ -1,14 +1,85 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Image,
+  FlatList,
+  TouchableOpacity,
+} from 'react-native';
+import {
+  initialCurrentLocation,
+  categoryData,
+  affordable,
+  fairPrice,
+  expensive,
+  restaurantData,
+} from '../data';
+
+import {COLORS, icons, images, SIZES, FONTS} from '../constants';
 
 const Home = () => {
-  return (
-    <View>
-      <Text>Home</Text>
-    </View>
-  );
+  function renderHeader() {
+    return (
+      <View style={{flexDirection: 'row', height: 50}}>
+        <TouchableOpacity
+          style={{
+            width: 50,
+            paddingLeft: SIZES.padding * 2,
+            justifyContent: 'center',
+          }}>
+          <Image
+            source={icons.nearby}
+            resizeMode="contain"
+            style={{
+              width: 30,
+              height: 30,
+            }}
+          />
+        </TouchableOpacity>
+        <View
+          style={{
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}>
+          <View
+            style={{
+              width: '70%',
+              height: '100%',
+              backgroundColor: COLORS.lightGray3,
+              alignItems: 'center',
+              justifyContent: 'center',
+              borderRadius: SIZES.radius,
+            }}>
+            <Text style={{...FONTS.h3}}> Location</Text>
+          </View>
+        </View>
+        <TouchableOpacity
+          style={{
+            width: 50,
+            paddingRight: SIZES.padding * 2,
+            justifyContent: 'center',
+          }}>
+          <Image
+            source={icons.basket}
+            resizeMode="contain"
+            style={{width: 30, height: 30}}
+          />
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
+  return <SafeAreaView>{renderHeader()}</SafeAreaView>;
 };
 
 export default Home;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.lightGray4,
+  },
+});
